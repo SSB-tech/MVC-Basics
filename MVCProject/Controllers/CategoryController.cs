258f0d297fad9ed9e06a73a2a5b99a310a2be710
Sushant <sushant.basnet@infodev.com.np>
2023-10-16 17:42:09 +0545

    MVC Project Basic

---
+++ MVCProject/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Data;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult ListAll()
+        {
+            var data = _db.Categories;
+            return View(data);
+        }
+        public IActionResult Create(Category model)
+        {
+
+        }
+
+    }
+}
---
+++ MVCProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject.Models;
+
+namespace MVCProject.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option):base(option)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
