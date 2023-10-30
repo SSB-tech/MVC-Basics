8373fdc70b5a32d499a08751a2c9d44cfeff243f
Sushant <sushant.basnet@infodev.com.np>
2023-10-30 12:35:36 +0545

    Server Side Validation

--- MVCProject/Controllers/CategoryController.cs
+++ MVCProject/Controllers/CategoryController.cs
@@ -21,5 +21,22 @@ namespace MVCProject.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and DisplayOrder Cannot be Same."); //Here error will be displayed below name property because we have used name as the key i.e  ModelState.AddModelError("Key", "Value")
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("ListAll");
+            }
+            return View(model);
+        }
+
     }
 }
