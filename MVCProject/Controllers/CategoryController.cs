70b538d84d1b35069ce7280c49428e31be15ac6e
Sushant <sushant.basnet@infodev.com.np>
2023-10-30 17:46:02 +0545

    MVC CRUD

--- MVCProject/Controllers/CategoryController.cs
+++ MVCProject/Controllers/CategoryController.cs
@@ -21,6 +21,35 @@ namespace MVCProject.Controllers
             return View();
         }
 
+        public IActionResult Edit(int id)
+        {
+           if(id == 0)
+            {
+                return NotFound();
+            }
+            var data = _db.Categories.Find(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return View(data);
+
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var data = _db.Categories.Find(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return View(data);
+
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category model)
@@ -32,6 +61,44 @@ namespace MVCProject.Controllers
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("ListAll");
+            }
+            return View(model);
+        }
+        //public IActionResult Delete(int id)
+        //{
+        //    return ();
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and DisplayOrder Cannot be Same."); //Here error will be displayed below name property because we have used name as the key i.e  ModelState.AddModelError("Key", "Value")
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(model);
+                _db.SaveChanges();
+                return RedirectToAction("ListAll");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category model)
+        {
+            if (model.Name == model.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and DisplayOrder Cannot be Same."); //Here error will be displayed below name property because we have used name as the key i.e  ModelState.AddModelError("Key", "Value")
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(model);
                 _db.SaveChanges();
                 return RedirectToAction("ListAll");
             }
